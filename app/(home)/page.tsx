ad9e8aeb45873943de4378430ffe277d9a359895
imamhossain71 <mehossainimam43@gmail.com>
2025-08-13 15:40:08 +0600

    04-create homepage carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
+import { HomeCarousel } from '@/components/ui/shared/home/home-carousal'
+import data from '@/lib/data'
+
 export default function Home() {
-  return (
-    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      <h1 className='h1-bold text-center p-10'>Home Page content</h1>
-    </div>
-  )
+  return <HomeCarousel items={data.carousels} />
 }
